4348ec695f262753390b94c93f32add897e2323e
dsdshcym <dsdshcym@gmail.com>
2016-05-17 20:09:19 +0800

    Print Products informations

--- lib/app.rb
+++ lib/app.rb
@@ -33,6 +33,33 @@ def puts_divider
   puts "********************"
 end
 
+products_hash["items"].each_with_index do |toy, i|
+  puts_divider if i > 0
+  puts
+  puts toy["title"]
+  puts_divider
+
+  retail_price = toy["full-price"].to_f
+  puts "Retail Price: $#{retail_price}"
+
+  total_purchases = toy["purchases"].count
+  puts "Total Purchases: $#{total_purchases}"
+
+  total_sum = toy["purchases"].map { |purchase| purchase["price"].to_f }.inject(&:+)
+  puts "Total Sales: $#{total_sum}"
+
+  average_price = total_sum / total_purchases
+  puts "Average Price: $#{average_price}"
+
+  average_discount = retail_price - average_price
+  puts "Average Discount: $#{average_discount}"
+
+  average_discount_percentage = (average_discount / retail_price * 100).round(2)
+  puts "Average Discount Percentage: #{average_discount_percentage}%"
+end
+
+puts
+
 	puts " _                         _     "
 	puts "| |                       | |    "
 	puts "| |__  _ __ __ _ _ __   __| |___ "
